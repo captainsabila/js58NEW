bdb8182d21ff1913e9803b7578d3c838d6a42768
captainsabila <sabila.demopoulos@gmail.com>
2017-06-21 16:59:34 -0400

    Update HW617app.js

--- HW617app.js
+++ HW617app.js
@@ -1,42 +1,28 @@
-/*I am unable to marry the timer to display a random image. I can start a timer upon clicking a button. 
-I can also generate an image upon clicking a button. */ 
-
-//starting a random timer, which unfortunately resets after hitting zero. 
-
 $(function(){
 	$('#random-timer-btn').on('click', function(event){
+	var randomTime = Math.ceil(Math.random() * (60 * 1000));
+	var seconds = Math.ceil(randomTime/1000);
+	$('#time').text(seconds);
 
-	function startTimer(duration, display) {
-    event.preventDefault();
-    var timer = duration, seconds;
-   
-
-    setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ?  seconds : seconds; 
-
-        display.text(seconds);
-
-        if (--timer < 0) {
-            timer = duration;
-            //var hoffPic = $('<img src="http://place-hoff.com/500/202">');
-			//$('#random-hoff').append(hoffPic);
-        }
-    }, 1000);
-}
-
-$(function ($) {
-        display = $('#time');
-    var randomTime = Math.ceil(Math.random() * 10000);
-    startTimer(randomTime, display);  
-})
-})
-})
-
-$(function(){
-	$('#random-timer-btn').on('click', function(event){
 	var hoffPic = $('<img src="http://place-hoff.com/500/202">');
-	$('#random-hoff').append(hoffPic)
+
+	var picAdd = function(){ 
+		$('#time').text(0);
+		$('#random-hoff').append(hoffPic);
+		clearInterval(clock);
+		clearInterval(randomTimer);
+	}
+
+	var randomTimer = setInterval(picAdd,randomTime);
+	var clock = setInterval(paintTime,1000);
+
+	function paintTime(){
+		seconds = seconds-1;
+		$('#time').text(seconds);
+		if(seconds <= 0){
+			clearInterval(clock);
+		}
+	}
+
 })
 })
